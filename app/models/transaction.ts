3f66f0eccb53f265f5068189331254252f8783cb
jampierzcode <jampierv127@gmail.com>
2024-12-02 11:02:41 -0500

    mejoras de compilacion entorno production

--- app/models/transaction.ts
+++ app/models/transaction.ts
@@ -1,5 +1,5 @@
 import { DateTime } from 'luxon'
-import { BaseModel, belongsTo, column, hasMany } from '@adonisjs/lucid/orm'
+import { BaseModel, belongsTo, column } from '@adonisjs/lucid/orm'
 import Subscription from './subscription.js'
 import type { BelongsTo } from '@adonisjs/lucid/types/relations'
 
